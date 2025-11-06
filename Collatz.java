4a7b02f1835515b2595be1c9ac91db27d822593a
Tom Friedberg <tom.friedberg@post.runi.ac.il>
2025-11-06 18:20:37 +0200

    assignment completed

--- Collatz.java
+++ Collatz.java
@@ -1,6 +1,43 @@
 // Demonstrates the Collatz conjecture.
 public class Collatz {
 	public static void main(String args[]) {
-	    // Replace this comment with your code
+
+	    int n = Integer.parseInt(args[0]);
+		String letter = args[1];
+		int n2 = 1;
+		int n3 = 1;
+		int steps = 1;
+		String sh = "";
+
+		while(n2<=n)
+		{
+			sh = sh + n2;
+			while(n2 != 1)
+				{
+				if(n2%2 == 0)
+				{
+					n2 = n2/2;
+				}
+				else 
+				{
+					n2 = n2*3 + 1;
+				}
+				sh = sh + " " + n2;
+				steps ++;
+			}
+			
+				if(letter.equals("v"))
+				{
+					System.out.println(sh + " (" + steps + ")");
+				}
+			
+			n3++;
+			n2 = n3;
+			sh = "";
+			steps = 1;
+	}
+
+	System.out.println(sh);
+
 	}
 }
--- TimeCalc.java
+++ TimeCalc.java
@@ -1,5 +1,30 @@
 public class TimeCalc {
     public static void main(String[] args) {
-        // Replace this comment with your code
+        
+        String time24h = args[0];
+        int MinToAdd = Integer.parseInt(args[1]);
+
+
+        int hours24 = Integer.parseInt(time24h.substring(0, 2)); 
+        int minutes = Integer.parseInt(time24h.substring(3, 5)); 
+
+
+        int totalMinutes = hours24*60 + minutes + MinToAdd;
+        int totalHours = totalMinutes/60;
+        int newHours = totalHours%24;
+        int newMinutes = totalMinutes - (totalHours*60);
+
+
+
+        if(newMinutes<10)
+        {
+            System.out.println(newHours + ":0" + newMinutes);
+        }
+
+        else
+        {
+        System.out.println(newHours + ":" + newMinutes);
+        }
+
     }
 }
